522070a6e2042917231ec2c8bbdb3ee7a46fdb5d
dmr-dj <didier.roche@lsce.ipsl.fr>
2024-02-27 22:41:11 +0100

    Created the skeleton for the main bash script and config files

---
+++ configs/dmrpwl/conf.gnu
@@ -0,0 +1,3 @@
+FC="/usr/bin/gfortran"
+NETCDFINC="/usr/include"
+NETCDFLIB="/usr/lib/x86_64-linux-gnu"
---
+++ main.sh
@@ -0,0 +1,135 @@
+#!/usr/bin/env bash
+
+# Copyright [2024] [Didier M. Roche]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+usage() {
+  cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}") [-h] [-v] [-f] -p param_value arg1 [arg2...]
+
+Script description here.
+
+Available options:
+
+-h, --help      Print this help and exit
+-v, --verbose   Print script debug info
+EOF
+  exit
+}
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+parse_params() {
+	
+  args=("$@")
+  
+  #~ [[ ${#args[@]} -lt 2 ]] && die "Missing script arguments: at the minimum I need one input and one output directory"
+
+  while :; do
+    case "${1-}" in
+    -h | --help) usage ;;
+    -v | --verbose) set -x ;;
+    --no-color) NO_COLOR=1 ;;
+    -?*) die "Unknown option: $1" ;;
+    *) break ;;
+    esac
+    shift
+  done
+
+  return 0
+}
+
+function split_string_to_array () {
+
+   arrayid=(`echo "$1" | tr "$2" " "`)
+   
+		
+} # end function
+
+# from: https://www.baeldung.com/linux/command-line-progress-bar
+bar_size=40
+bar_char_done="#"
+bar_char_todo="-"
+bar_percentage_scale=2
+
+function show_progress {
+    current="$1"
+    total="$2"
+
+    # calculate the progress in percentage 
+    percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+    # The number of done and todo characters
+    done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
+    todo=$(bc <<< "scale=0; $bar_size - $done" )
+
+    # build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+
+    # output the bar
+    echo -ne "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%"
+
+    if [ $total -eq $current ]; then
+        echo -e "\nDONE"
+    fi
+}
+
+parse_params "$@"
+setup_colors
+
+# script logic here
+
+msg "${ORANGE}Read parameters:${NOFORMAT}"
+msg "- arguments: ${args[*]-}"
+
+
+
+tempDIR="tmp-$(hexdump -n 6 -v -e '/1 "%02X"' /dev/urandom)"
+
+msg "${RED} ${tempDIR} to be created ${NOFORMAT}"
+
+
+msg "${GREEN}Finalized work${NOFORMAT}"
+msg "${GREEN}== Final files are in: ${NOFORMAT}"
+
+msg "${NOFORMAT}"
+
+# The End of All Things (op. cit.)
