c111e0b36af014c70d8177df32cf448698c6c1d6
dmr-dj <didier.roche@lsce.ipsl.fr>
2024-03-12 18:43:49 +0100

    Added a preliminary install for makefile/FF/ad-hoc

--- src/build_pkg.sh
+++ src/build_pkg.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 function build_pkg_cc () {
 	
   local method=${1//\"/}
@@ -39,7 +40,108 @@ function build_pkg_cc () {
 
 function build_pkg_ff () {
 	
-	die "Building with language FF is not implemented yet"
+  local -n tomlinput=${1}	
+  local method=${2//\"/}
+    
+   # To check the hash table content
+   for i in "${!tomlinput[@]}"
+   do
+     echo "${i} ${tomlinput[$i]}"
+   done
+    
+    
+  case ${method} in 
+    make)
+     vrb "Attmpt. ${method} on ${PKG_NAME}"
+     
+     place_to_be="${tempDIR}/${PKG_NAME}/${tomlAA["location"]//\"/}"
+     hereiam=$(pwd)
+     
+     if [ -d ${place_to_be} ]
+     then
+        cd ${place_to_be}
+        
+        sub_method=${tomlAA["mkfile"]//\"/}
+        case ${sub_method} in
+        ad-hoc)
+          vrb "Going for ad-hoc mkfile"
+          # Here analyse the Makefile.LIPaS
+          if [ -f Makefile.LIPaS ]
+          then
+            vrb "Scan variables in Makefile.LIPaS"
+            
+            mkefile="Makefile.LIPaS"
+            dctfile="${DICT_FOR_ENV}"
+                        
+            readarray -d '#' -t mkefile_array < <(cat ${mkefile} | grep --null -n \@.*\@ | cut --delimiter=: -f2 | tr '\n' '#')  
+            readarray -d '#' -t dctfile_array < <(cat ${dctfile} | grep --null -n \@.*\@ | cut --delimiter=: -f2 | tr '\n' '#')
+            
+            mkefileinwork="Makefile.ins"
+            
+            cp -f ${mkefile} ${mkefileinwork}
+            
+            for (( j = 0 ; j < ${#mkefile_array[@]} ; j++ ))
+            do
+              key_mkefile=$( get_keyvalue "${mkefile_array[j]}" )
+              value_mkefile=$( get_valuekey "${mkefile_array[j]}" )
+              
+              for (( k = 0 ; k < ${#dctfile_array[@]} ; k++ ))
+              do              
+                key_dctfile=$( get_keyvalue "${dctfile_array[k]}" )
+
+                if [ ${key_dctfile} == ${value_mkefile} ]
+                then
+                   value_dctfile=$( get_valuekey "${dctfile_array[k]}" )
+                   
+                   if [[ "${value_dctfile}" == "FROM_ENV" ]]
+                   then
+                      # Hardwired for now, could do much better
+                      case ${key_dctfile//\@/} in
+                       FORTRAN_COMPILER_PATH)
+                        value_dctfile="${FC}"
+                       ;;
+                       INSTALL_PREFIX)
+                        value_dctfile="${LIPaS_ROOT}"
+                       ;;
+                       *)
+                        die "Unkown key from env ${key_dctfile}"
+                       ;;
+                      esac 
+                   fi
+                 #~ else
+                   #~ vrb "${key_dctfile} and ${value_mkefile} not comparable"
+                   
+                   # Replace values found in the temporary makefile
+                   sed -i "s+${key_dctfile}+${value_dctfile}+g" ${mkefileinwork}
+                 fi
+              done
+            done
+
+          else
+            die "Could not find the Makefile.LIPaS file, update toml file"
+          fi 
+        ;;
+        *)
+          die "Unknown makefile methodologies"
+        ;;
+        esac
+        
+        make --makefile=${mkefileinwork} clean 2>&1 > /dev/null        
+        make --makefile=${mkefileinwork} ${tomlAA["target"]//\"/}
+        make --makefile=${mkefileinwork} install
+        
+        cd ${hereiam}
+     else
+       die "Missing location directory: ${place_to_be}"
+     fi
+          
+     success_or_not=1
+     return ${success_or_not}
+    ;;
+	*)
+	die "Unknown build meth. ${method} for lang=CC"
+	;;
+  esac
 
 }
 
@@ -58,13 +160,13 @@ function build_pkg () {
         return $?
      ;;
      FF)
-        build_pkg_ff ${tomlAA["method"]}     
+        build_pkg_ff tomlAA ${tomlAA["method"]}     
      ;;
      *)
      die "Unhandled programming language"
      ;;
    esac
-   echo ${!tomlAA[@]}
+   return $?
 
 }
 
