1bda30fdfce81da5e4ee5b3a0040ffbae85e217e
dmr-dj <didier.roche@lsce.ipsl.fr>
2024-03-21 15:33:15 +0100

    Updated version accordingly.

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ set -Eeuo pipefail
 trap cleanup SIGINT SIGTERM ERR EXIT
 
 prog_name="LIPaS"
-script_version="0.3.3"
+script_version="0.3.4"
 
 MAIN_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
